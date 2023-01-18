cc5e1ad57dac1572013869088d017ead93080f4d
Google Jr <andilembele020@gmail.com>
2023-01-18 14:42:16 +0200

    server.js file changes

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import routes from './routes';
-import middleware from './middleware';
+// import routes from './routes';
+// import middleware from './middleware';
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.use(middleware.logger);
-app.use(middleware.authenticator);
-app.use(routes);
-app.use(middleware.errorHandler);
+// app.use(middleware.logger);
+// app.use(middleware.authenticator);
+// app.use(routes);
+// app.use(middleware.errorHandler);
 
 module.exports = app;
