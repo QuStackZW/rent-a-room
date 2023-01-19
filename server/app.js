2b5b4e79dda2250ab12edad94db6c6523f997793
Google Jr <andilembele020@gmail.com>
2023-01-19 14:00:35 +0200

    restructured folders

---
+++ server/app.js
@@ -0,0 +1,27 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL = process.env.ATLAS_URI;
+const PORT = process.env.PORT || 5001;
+
+mongoose.set("strictQuery", false);
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+});
