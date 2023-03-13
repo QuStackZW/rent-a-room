744d16e6ed4f7eafad4d5a5a8bac46a3a20bf167
Google Jr <andilembele020@gmail.com>
2023-03-13 05:08:38 +0200

    Renaming Renter to Tenant

--- server/src/models/Tenant.js
+++ server/src/models/Tenant.js
@@ -10,9 +10,6 @@ const renterSchema = new Schema({
     phoneNumber: { type: String, required: true, unique: true },
     nationalId: { type: String, required: true, unique: true },
     address: { type: String, required: true },
-    properties: [
-        { type: mongoose.Types.ObjectId, ref: "Property", required: true },
-    ],
     password: { type: String, required: true, minlength: 8 },
     createdAt: { type: Date, default: Date.now },
 });
