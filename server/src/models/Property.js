151ee63254b66fe2d80cd36525d655f7247ecea5
Google Jr <andilembele020@gmail.com>
2023-03-13 04:58:28 +0200

    modifying the property model

--- server/src/models/Property.js
+++ server/src/models/Property.js
@@ -3,7 +3,10 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const propertySchema = new Schema({
-    location: { type: String, required: true },
+    location: { type: String, required: false },
+    streetAddress: { type: String, required: true },
+    suburb: { type: String, required: true },
+    city: { type: String, required: true },
     size: { type: String, required: true },
     price: { type: String, required: true },
     description: { type: String, required: true },
